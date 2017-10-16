19367eb2a032e427487ea5e1260be02dcac54a2d
Nikola Kolev <koue@chaosophia.net>
2017-10-16 23:00:16 +0200

    Use getdtablecount implementation from the FreeBSD src tree.

--- src/usr.sbin/httpd/getdtablecount.c
+++ src/usr.sbin/httpd/getdtablecount.c
@@ -1,76 +1,62 @@
-/*
- * Copyright (c) 2015-2016 Nikola Kolev
+/*-
+ * Copyright (c) 2015 Craig Rodrigues
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
- *    - Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *    - Redistributions in binary form must reproduce the above
- *      copyright notice, this list of conditions and the following
- *      disclaimer in the documentation and/or other materials provided
- *      with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
-#ifndef HAVE_GETDTABLECOUNT
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/param.h>
-#include <sys/socket.h>
-#include <sys/sysctl.h>
-#include <sys/queue.h>
 #include <sys/types.h>
-#include <sys/user.h>
-#include <libprocstat.h>
+#include <sys/sysctl.h>
+#include <stddef.h>
 
-#include "getdtablecount.h"
-
-int
-getdtablecount(void) {
-	struct procstat *procstat;
-	struct kinfo_proc *kipp;
-	struct filestat_list *head;
-	struct filestat *fst;
-	unsigned int fd_count = 0;
-	pid_t pid;
-	unsigned int cnt;
-
-	procstat = procstat_open_sysctl();
-	if (procstat == NULL)
-		return 0;
-
-	pid = getpid();
-	
-	kipp = procstat_getprocs(procstat, KERN_PROC_PID, pid, &cnt);
-	if (kipp == NULL)
-		return 0;
-
-	if (cnt != 0) {
-		head = procstat_getfiles(procstat, kipp, 0);
-		if (head == NULL)
-			return 0;
-		fst = STAILQ_LAST(head, filestat, next);
-		fd_count = fst->fs_fd;
-	}
-	procstat_freeprocs(procstat, kipp);
-	return fd_count;
-}
-
+#ifndef KERN_PROC_NFDS
+#define	KERN_PROC_NFDS		43	/* number of open file descriptors */
 #endif
+
+int getdtablecount(void);
+
+/*
+ * Return the count of open file descriptors for this process.
+ *
+ */
+int
+getdtablecount(void)
+{
+	int mib[4];
+	int error;
+	int nfds;
+	size_t len;
+
+	len = sizeof(nfds);
+	mib[0] = CTL_KERN;
+	mib[1] = KERN_PROC;
+	mib[2] = KERN_PROC_NFDS;
+	mib[3] = 0;
+
+	error = sysctl(mib, 4, &nfds, &len, NULL, 0);
+	if (error)
+		return (-1);
+	return (nfds);
+}
